a4c811a3ee3d6c487581a6046fea32935f9cc6c0
Rameez Ul Hassan <ulhassanr59@gmail.com>
2024-02-13 22:51:51 +0500

    create landing page

--- landing-page/app/page.tsx
+++ landing-page/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Card from "./component/card";
+import Footer from "./component/Footer";
 
 export default function Home() {
   return (
@@ -12,53 +14,68 @@ export default function Home() {
             width={"800"}
             height={"800"}
           />
-          {/* <div className="absolute inset-0 flex items-center justify-center z-10">
-        <h1 className="text-4xl font-bold text-white">Your Text Here</h1>
-        </div> */}
+
           <div className="  absolute inset-x-72 inset-y-12 text-white font-semibold ">
-            <p className=" text-5xl">
+            <p className=" lg:text-5xl ">
               Hi,<span className="text-2xl"> I am Rameez Ul Hassan</span>
             </p>
-            
-            <p className="text-5xl mt-5">
-            Full Stack Developer
-            </p>
+
+            <p className="text-5xl mt-5">Full Stack Developer</p>
           </div>
         </div>
-        <div className="bg-black text-white text-center p-12 text-5xl font-semibold">
+        <div className="bg-black text-white text-center p-12 lg:text-5xl font-semibold sm:text-4xl md:text-3xl">
           Work, I Can Do For You
         </div>
-        <div className=" flex justify-around m-12 mx-28 ">
-        <div className="w-80 shadow-md shadow-gray-400 rounded-md">
-          <Image className="mb-5"
-          src={"/images/web.png"}
-          alt="Image loading..."
-          width={"320"}
-          height={"300"}
+        <div className=" flex justify-around flex-wrap lg:m-12 lg:mx-28">
+          <Card
+            image={"/images/web.png"}
+            title={"Web Development"}
+            Description={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, molestias! In quis sunt reiciendis necessitatibus cum."
+            }
           />
-          <h1 className="text-lg font-bold mb-3 ml-5">Web Development</h1>
-          <p className="mx-5 mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, molestias! In quis sunt reiciendis necessitatibus cum. </p>
-        </div>
-        <div className="w-80 shadow-md shadow-gray-400 rounded-md">
-          <Image className="mb-5"
-          src={"/images/mobile.png"}
-          alt="Image loading..."
-          width={"320"}
-          height={"300"}
+          <Card
+            image={"/images/mobile.png"}
+            title={"App Development"}
+            Description={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, molestias! In quis sunt reiciendis necessitatibus cum."
+            }
           />
-          <h1 className="text-lg font-bold mb-3 ml-5">App Development</h1>
-          <p className="mx-5 mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, molestias! In quis sunt reiciendis necessitatibus cum. </p>
-        </div>
-        <div className="w-80 shadow-md shadow-gray-400 rounded-md">
-          <Image className="mb-5"
-          src={"/images/uiux.png"}
-          alt="Image loading..."
-          width={"320"}
-          height={"300"}
+          <Card
+            image={"/images/uiux.png"}
+            title={"UIUX Development"}
+            Description={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, molestias! In quis sunt reiciendis necessitatibus cum."
+            }
           />
-          <h1 className="text-lg font-bold mb-3 ml-5">UIUX Development</h1>
-          <p className="mx-5 mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, molestias! In quis sunt reiciendis necessitatibus cum. </p>
         </div>
+        <div className="flex text-white ">
+          <div className="bg-yellow-700 p-10">
+        <Footer
+          
+          
+          title={"Contact Me"}
+          paraDetails={
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi dolor, esse quasi doloremque temporibus quis."
+          }
+          mail={"abc@gmail.com"}
+        />
+        </div>
+        <div className="bg-red-900 p-10">
+        <Footer
+          
+          
+          title={"About Me"}
+          paraDetails={
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi dolor, esse quasi doloremque temporibus quis."
+          }
+          
+          
+        />
+        </div>
+        </div>
+        <div  className="bg-black text-white text-lg text-center p-4">
+          <p>PROJECT BY RAMEEZ UL HASSAN</p>
         </div>
       </div>
     </main>
