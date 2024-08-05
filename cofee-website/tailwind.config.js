1663255f41a7db1b5542443c9825f203ae67b44d
Rameez Ul Hassan <ulhassanr59@gmail.com>
2024-08-05 12:09:54 +0500

    cofee website

---
+++ cofee-website/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors:{
+        'darkYellow':['#C06B3E']
+      },
+      fontFamily:{
+        'Poppins':["Poppins","Arial"]
+      }
+    },
+  },
+  plugins: [],
+}
